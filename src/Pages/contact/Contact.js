f403577ec5e3459468c37cf80b331edbd611d08a
Hamo Muradyan <hamomuradyan93@gmail.com>
2024-09-03 12:32:10 +0400

    change layouts for contactUs

--- src/Pages/contact/Contact.js
+++ src/Pages/contact/Contact.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Header from "./components/header";
 import Feedback from "./components/feedback";
@@ -10,8 +12,20 @@ function Contact() {
       <title>Contact Us | PaladiumTechServiceLimited </title>
       <Header />
       <Row xs={1} md={2} className="g-4">
-        <Feedback className="feedback" />
-      </Row>
+          <Col className="emailcol">
+            <Card>
+              <Card.Body>
+                <Feedback />
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card>
+              <Card.Body>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
     </div>
   );
 }
